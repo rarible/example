80fac56b0ef1f27eb5a6774b48e0a018b5a593ac
Sergei Vychegzhanin <s.vychegzhanin@rarible.com>
2021-12-21 16:02:53 +0500

    fix: formatting

--- src/connector/ethereum/fortmatic.ts
+++ src/connector/ethereum/fortmatic.ts
@@ -1,9 +1,9 @@
 import {combineLatest, defer, from, Observable} from "rxjs"
-import type { WidgetMode } from "fortmatic/dist/cjs/src/core/sdk"
+import type {WidgetMode} from "fortmatic/dist/cjs/src/core/sdk"
 import {first, map, mergeMap, startWith} from "rxjs/operators"
-import { AbstractConnectionProvider, ConnectionState } from "../provider"
-import { EthereumWallet } from "./domain"
-import { Maybe } from "../../common/maybe"
+import {AbstractConnectionProvider, ConnectionState} from "../provider"
+import {EthereumWallet} from "./domain"
+import {Maybe} from "../../common/maybe"
 import Web3 from "web3"
 
 type FM = WidgetMode
@@ -17,21 +17,21 @@ export class FortmaticConnectionProvider extends AbstractConnectionProvider<"for
 	) {
 		super()
 		this.fortmatic = cache(() => this._connect())
-        this.connection = defer(() => this.fortmatic.pipe(
-            mergeMap(() => promiseToObservable(this.getWallet())),
-            map(wallet => {
-                if (wallet) {
-                    return { status: "connected" as const, connection: wallet }
-                } else {
-                    return undefined
-                }
-            }),
-            startWith({ status: "connecting" as const }),
-        ))
+		this.connection = defer(() => this.fortmatic.pipe(
+			mergeMap(() => promiseToObservable(this.getWallet())),
+			map(wallet => {
+				if (wallet) {
+					return {status: "connected" as const, connection: wallet}
+				} else {
+					return undefined
+				}
+			}),
+			startWith({status: "connecting" as const}),
+		))
 	}
 
 	private async _connect(): Promise<FM> {
-		const { default: Fortmatic } = await import("fortmatic")
+		const {default: Fortmatic} = await import("fortmatic")
 		return new Fortmatic(this.apiKey) //todo all options?
 	}
 
@@ -56,25 +56,25 @@ export class FortmaticConnectionProvider extends AbstractConnectionProvider<"for
 		return true === await sdk.user.isLoggedIn()
 	}
 
-    private async getWallet(): Promise<Observable<EthereumWallet | undefined>> {
-        const sdk = await this.fortmatic.pipe(first()).toPromise()
-        const provider = sdk.getProvider()
-        const web3 = new Web3(provider as any)
+	private async getWallet(): Promise<Observable<EthereumWallet | undefined>> {
+		const sdk = await this.fortmatic.pipe(first()).toPromise()
+		const provider = sdk.getProvider()
+		const web3 = new Web3(provider as any)
 
 		const accounts = web3.eth.getAccounts();
 		const chainId = web3.eth.getChainId();
 
-        return combineLatest([accounts, chainId]).pipe(
-            map(([accounts, chainId]) => {
+		return combineLatest([accounts, chainId]).pipe(
+			map(([accounts, chainId]) => {
 				const address = accounts[0]
-                if (address) {
-                    return { chainId, address, provider }
-                } else {
-                    return undefined
-                }
-            }),
-        )
-    }
+				if (address) {
+					return {chainId, address, provider}
+				} else {
+					return undefined
+				}
+			}),
+		)
+	}
 }
 
 function cache<T>(fn: () => Promise<T>): Observable<T> {
@@ -93,7 +93,7 @@ function cache<T>(fn: () => Promise<T>): Observable<T> {
 }
 
 function promiseToObservable<T>(promise: Promise<Observable<T>>): Observable<T> {
-    return from(promise).pipe(
-        mergeMap(it => it),
-    )
+	return from(promise).pipe(
+		mergeMap(it => it),
+	)
 }
